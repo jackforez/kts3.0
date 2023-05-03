8402968da7e24257a7f4bfc7d8d7413f52f7a3af
Jack <jackforez@gmail.com>
2023-05-04 00:15:46 +0700

    v

--- src/pages/Bills.jsx
+++ src/pages/Bills.jsx
@@ -7,7 +7,7 @@ import ReactToPrint, { useReactToPrint } from "react-to-print";
 import { ktsRequest } from "../ultis/connections";
 import { logout } from "../redux/userSlice";
 import { Button, GridData, Input } from "../components";
-import { printer, search, trash } from "../ultis/svgs";
+import { excel, printer, search, trash } from "../ultis/svgs";
 import { search as myFilter, toVND } from "../ultis/functions";
 import logo from "../assets/logo.svg";
 import { loaded, onLoading, onRefreh } from "../redux/systemSlice";
@@ -69,10 +69,15 @@ const Bills = () => {
     }
   };
   const headers = [
-    { title: "Đơn hàng ", size: "w-3/12" },
-    { title: "chi tiết", size: "w-8/12" },
-    { title: "Thao tác", size: "w-1/12" },
+    { title: "Đơn hàng ", size: "w-2/12" },
+    { title: "người nhận", size: "w-3/12" },
+    { title: "người gửi", size: "w-3/12" },
+    { title: "COD", size: "w-1/12 text-end" },
+    { title: "cước", size: "w-1/12 text-end" },
+    { title: "tổng thu", size: "w-1/12 text-end" },
+    { title: "Thao tác", size: "w-1/12 text-end" },
   ];
+  console.log(query);
   return (
     <div className="p-3">
       {showPrint && <ToPrint data={dataPrint} setClose={setShowPrint} />}
@@ -85,16 +90,34 @@ const Bills = () => {
           padding="sm"
         />
         <div className="text-xs text-start flex justify-start capitalize flex-1 gap-2">
-          <span className="inline-block bg-white px-2 pt-0.5 pb-1 rounded-md border border-ktsPrimary cursor-pointer hover:bg-ktsPrimary hover:text-white">
+          <span
+            className="inline-block bg-white px-2 pt-0.5 pb-1 rounded-md border border-ktsPrimary cursor-pointer hover:bg-ktsPrimary hover:text-white"
+            onClick={() => {
+              setQuery("");
+            }}
+          >
             Tất cả ({bills.length})
           </span>
-          <span className="inline-block bg-white px-2 pt-0.5 pb-1 rounded-md border border-red-500 cursor-pointer hover:bg-red-500 text-red-500 hover:text-white">
+          <span
+            className="inline-block bg-white px-2 pt-0.5 pb-1 rounded-md border border-red-500 cursor-pointer hover:bg-red-500 text-red-500 hover:text-white"
+            onClick={() => setQuery("hủy")}
+          >
             hủy (1)
           </span>
-          <span className="inline-block bg-white px-2 pt-0.5 pb-1 rounded-md border border-green-500 cursor-pointer hover:bg-green-500 text-green-500 hover:text-white">
+          <span
+            className="inline-block bg-white px-2 pt-0.5 pb-1 rounded-md border border-green-500 cursor-pointer hover:bg-green-500 text-green-500 hover:text-white"
+            onClick={() => {
+              setQuery("thành công");
+            }}
+          >
             thành công(10)
           </span>
-          <span className="inline-block bg-white px-2 pt-0.5 pb-1 rounded-md border border-blue-500 cursor-pointer hover:bg-blue-500 text-blue-500 hover:text-white">
+          <span
+            className="inline-block bg-white px-2 pt-0.5 pb-1 rounded-md border border-blue-500 cursor-pointer hover:bg-blue-500 text-blue-500 hover:text-white"
+            onClick={() => {
+              setQuery("đang giao");
+            }}
+          >
             đang giao(2)
           </span>
         </div>
@@ -103,9 +126,22 @@ const Bills = () => {
           <span>to</span>
           <input type="date" name="toDate" id="toDate" className="px-2" />
         </div> */}
+        <Button type={"outline-primary"} padding={"sm"} title="under construct">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="21"
+            height="21"
+            x="0"
+            y="0"
+            viewBox="0 0 50 50"
+            className="w-5 h-5"
+          >
+            <path d={excel}></path>
+          </svg>
+        </Button>
         <Link
           to="/dashboard/bills/new"
-          className="order-2 flex items-center gap-1 rounded border border-primary-600 p-1.5 text-xs text-primary-600 hover:bg-primary-600 hover:text-white md:text-base lg:order-3"
+          className="order-2 flex items-center bg-white gap-1 rounded border border-primary-600 p-1.5 text-xs text-primary-600 hover:bg-primary-600 hover:text-white md:text-base lg:order-3"
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -127,28 +163,25 @@ const Bills = () => {
       </div>
       <div className="border border-ktsPrimary rounded-md">
         <GridData headers={headers}>
-          <div className="divide-y divide-dashed divide-ktsPrimary bg-white shadow-lg rounded-md">
-            {myFilter(bills, query, ["toPhone", "toName"]).length > 0 ? (
-              myFilter(bills, query, ["toPhone", "toName"]).map((b, i) => {
-                return (
-                  <div className="px-2 py-1.5 flex items-center" key={i}>
-                    <div className="w-1/4">
-                      <div className="space-x-3">
-                        <span className="bg-primary-200 px-2 py-0.5 rounded text-primary-700 font-semibold text-sm">
+          <div className="divide-y text-sm divide-dashed divide-ktsPrimary bg-white shadow-lg rounded-md">
+            {myFilter(bills, query, ["toPhone", "toName", "status"]).length >
+            0 ? (
+              myFilter(bills, query, ["toPhone", "toName", "status"]).map(
+                (b, i) => {
+                  return (
+                    <div className="px-2 py-1.5 flex items-center" key={i}>
+                      <div className="w-2/12">
+                        <span className="bg-primary-200 px-1 inline-block py-0.5 rounded text-primary-700 font-semibold text-xs">
                           {b.status}
                         </span>
                         <span>{b.orderNumber}</span>
+
+                        {/* <div>Mã tra cứu: {b.partnerTrackingId}</div> */}
                       </div>
-                      <div>Mã tra cứu: {b.partnerTrackingId}</div>
-                      <div className="font-semibold">
-                        Tiền cước: {toVND(b.ktsAmount)}
-                      </div>
-                    </div>
-                    <div className="w-8/12 md:grid md:auto-cols-fr md:grid-flow-col">
-                      <div className="">
-                        <div>Người nhận: {b.toName}</div>
-                        <div>Điện thoại: {b.toPhone}</div>
-                        <div>
+                      {/* <div className="w-/12 md:grid md:auto-cols-fr md:grid-flow-col"> */}
+                      <div className="w-3/12">
+                        <span>{b.toName + " - " + b.toPhone}</span>
+                        {/* <div>
                           Địa chỉ:{" "}
                           {b.toAddress +
                             ", " +
@@ -157,46 +190,47 @@ const Bills = () => {
                             b.toDistrict +
                             ", " +
                             b.toCity}
-                        </div>
+                        </div> */}
                       </div>
-                      <div className="">
-                        <div>Người nhận: {b.fromName}</div>
-                        <div>Điện thoại: {b.fromPhone}</div>
-                        <div>
-                          Địa chỉ:{" "}
-                          {b.fromAddress +
-                            ", " +
-                            b.fromWard +
-                            ", " +
-                            b.fromDistrict +
-                            ", " +
-                            b.fromCity}
-                        </div>
+                      <div className="w-3/12">
+                        <span>{b.fromName + " - " + b.fromPhone}</span>
+                      </div>
+                      <div className="w-1/12 text-end">
+                        <span>{toVND(b.cod)}</span>
+                      </div>
+                      <div className="w-1/12 text-end">
+                        <span>{toVND(b.ktsAmount - b.cod)}</span>
+                      </div>
+                      <div className="w-1/12 text-end">
+                        <span className="font-semibold">
+                          {toVND(b.ktsAmount)}
+                        </span>
+                      </div>
+                      {/* </div> */}
+                      <div className="w-1/12 flex justify-between md:justify-end md:gap-2">
+                        <Button
+                          type="outline-primary"
+                          icon={printer}
+                          iconSize={"4"}
+                          title={"In vận đơn"}
+                          padding={"xs"}
+                          callback={(e) => {
+                            setShowPrint(true);
+                            setDataPrint(JSON.stringify(b));
+                          }}
+                        ></Button>
+                        <Button
+                          type="outline-danger"
+                          icon={trash}
+                          iconSize={"4"}
+                          title={"Hủy vận đơn"}
+                          padding={"xs"}
+                        ></Button>
                       </div>
                     </div>
-                    <div className="w-1/12 flex justify-between md:justify-center md:gap-2">
-                      <Button
-                        type="outline-primary"
-                        icon={printer}
-                        iconSize={"4"}
-                        title={"In vận đơn"}
-                        padding={"sm"}
-                        callback={(e) => {
-                          setShowPrint(true);
-                          setDataPrint(JSON.stringify(b));
-                        }}
-                      ></Button>
-                      <Button
-                        type="outline-danger"
-                        icon={trash}
-                        iconSize={"4"}
-                        title={"Hủy vận đơn"}
-                        padding={"sm"}
-                      ></Button>
-                    </div>
-                  </div>
-                );
-              })
+                  );
+                }
+              )
             ) : (
               <div className="py-3 text-center">
                 {loading ? (
