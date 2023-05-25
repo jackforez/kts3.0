023ca0c3c176956cb4a8778ae2abc8a0461488d8
JackForez <jackforez@gmail.com>
2023-05-25 20:57:13 +0700

    v

--- src/pages/Config.jsx
+++ src/pages/Config.jsx
@@ -21,24 +21,31 @@ const Config = () => {
   const token = currentUser.token;
   const [file, setFile] = useState([]);
   const [urls, setUrls] = useState([]);
-  const [dlFile, setDlFile] = useState(null);
+  const [dlFile, setDlFile] = useState();
   const [downloadUrl, setDownloadUrl] = useState("");
+  const [qrUrl, setQrUrl] = useState("");
   const [isLoadedFiles, setIsLoadedFiles] = useState(false);
   const [percs, setPercs] = useState(0);
+  const [editLink, setEditLink] = useState(false);
   const [currentParams, setCurrentParams] = useState({});
   const [currentBgs, setCurrentBgs] = useState([]);
+  const [currentQR, setCurrentQR] = useState("");
   useEffect(() => {
     const fetchData = async () => {
       try {
         const res = await ktsRequest.get("/system");
         setCurrentParams(res.data);
         setCurrentBgs(res.data.homeBackgroundImages);
+        setCurrentQR(res.data.qr);
+        setQrUrl(res.data.link);
+        console.log(currentQR);
       } catch (error) {
         toast.error("Không get được thông số hệ thống");
       }
     };
     fetchData();
   }, [refresh]);
+
   useEffect(() => {
     setIsLoadedFiles(false);
     const uploadFile = async () => {
@@ -65,37 +72,31 @@ const Config = () => {
         })
       );
       setIsLoadedFiles(true);
-      setPercs(100);
+      // setPercs(100);
     };
     file && uploadFile();
   }, [file]);
   useEffect(() => {
     setIsLoadedFiles(false);
     const uploadFile = async () => {
-      setDlFile(null);
-      setPercs(0);
-
-      file.map((f, i) => {
-        const name = new Date().getTime() + currentUser._id + "_" + f.name;
-        const storageRef = ref(storage, `setup/${name}`);
-        const uploadTask = uploadBytesResumable(storageRef, f);
-
-        uploadTask.on(
-          "state_changed",
-          (snapshot) => {},
-          (error) => {
-            console.log(error);
-          },
-          () => {
-            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-              setDownloadUrl(downloadURL);
-            });
-          }
-        );
-      });
-
+      dispatch(onLoading());
+      const name = new Date().getTime() + currentUser._id + "_" + dlFile.name;
+      const storageRef = ref(storage, `setup/${name}`);
+      const uploadTask = uploadBytesResumable(storageRef, dlFile);
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {},
+        (error) => {
+          console.log(error);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            setDownloadUrl(downloadURL);
+          });
+        }
+      );
       setIsLoadedFiles(true);
-      setPercs(100);
+      dispatch(loaded());
     };
     dlFile && uploadFile();
   }, [dlFile]);
@@ -115,7 +116,12 @@ const Config = () => {
     await ktsRequest
       .put(
         "/system",
-        { ...currentParams, homeBackgroundImages: [...currentBgs, ...urls] },
+        {
+          ...currentParams,
+          homeBackgroundImages: [...currentBgs, ...urls],
+          link: qrUrl,
+          qr: downloadUrl,
+        },
         {
           headers: {
             Authorization: `Bearer ${token}`,
@@ -296,71 +302,132 @@ const Config = () => {
           </Button>
         </div>
         <div className="flex w-full items-center">
-          <div className="max-w-3/4 flex items-center rounded-md border border-dashed border-ktsPrimary p-1.5 overflow-x-auto">
+          <div className="max-w-full flex items-center rounded-md border border-dashed border-ktsPrimary p-1.5 overflow-x-auto">
             {/* {file?.length > 0 ? ( */}
             <div className="flex gap-1.5">
-              <div className="w-36 relative overflow-hidden">
-                {dlFile ? (
-                  <div className="w-36 h-24 overflow-hidden flex rounded bg-white">
-                    <img
-                      src={dlFile}
-                      alt=""
-                      className="object-cover w-full h-auto"
-                    />
-                    {/* <div
-                      className={`flex justify-between absolute bottom-0 w-[${percs}%] h-4 bg-primary rounded-b-md`}
-                    ></div> */}
+              <div className="w-24 relative overflow-hidden">
+                {downloadUrl || currentParams.qr ? (
+                  <div className="min-w-1/4 w-24 relative overflow-hidden">
+                    <div className="w-24 h-24 overflow-hidden flex rounded bg-white">
+                      <img
+                        src={downloadUrl || currentParams.qr}
+                        alt=""
+                        className="object-cover w-full h-auto"
+                      />
+                    </div>
+
+                    <button
+                      className="hover:text-white hover:bg-red-500 p-1 rounded-full absolute top-0 right-0"
+                      onClick={() => {
+                        handleDeleteFireBase(downloadUrl);
+                        setDlFile(null);
+                        setDownloadUrl("");
+                      }}
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={3}
+                        stroke="currentColor"
+                        className="w-3 h-3"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M6 18L18 6M6 6l12 12"
+                        />
+                      </svg>
+                    </button>
+
+                    {isLoadedFiles && (
+                      <div
+                        className={`absolute bottom-0 p-0.5 bg-green-500 rounded`}
+                      >
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          strokeWidth={1.5}
+                          stroke="white"
+                          className="w-4 h-4"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M4.5 12.75l6 6 9-13.5"
+                          />
+                        </svg>
+                      </div>
+                    )}
                   </div>
                 ) : (
-                  <button
-                    className="hover:text-white hover:bg-red-500 p-1 rounded-full absolute top-0 right-0"
-                    onClick={() => {
-                      handleDeleteFireBase(downloadUrl);
-                      setDownloadUrl("");
-                    }}
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth={3}
-                      stroke="currentColor"
-                      className="w-3 h-3"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M6 18L18 6M6 6l12 12"
-                      />
-                    </svg>
-                  </button>
+                  <div className="w-24 h-24 flex items-center justify-center">
+                    <Button
+                      type={"primary"}
+                      size="rounded-full"
+                      icon={add}
+                      padding={"md"}
+                      disabledBy={loading}
+                      animation={true}
+                      loading={loading}
+                      callback={() => {
+                        document.getElementById("downloadInput").click();
+                      }}
+                    ></Button>
+                  </div>
                 )}
               </div>
             </div>
-            <div className="w-36 h-24 flex items-center justify-center">
-              <Button
-                type={"primary"}
-                size="rounded-full"
-                icon={add}
-                padding={"md"}
-                disabledBy={loading}
-                animation={true}
-                loading={loading}
-                callback={() => {
-                  document.getElementById("downloadInput").click();
-                  console.log(dlFile);
-                }}
-              ></Button>
-            </div>
+
             <input
               type="file"
               multiple
               id="downloadInput"
               hidden
               onChange={(e) => {
-                setFile(Array.from(e.target.files[0]));
+                setDlFile(e.target.files[0]);
               }}
             />
+          </div>
+
+          <div className="pl-2 space-y-2 flex-1">
+            <div className="gap-2 flex">
+              {/* <Input
+                placehoder="Tên đăng nhập . . ."
+                type="text"
+                icon={userName}
+                value={username}
+                onChange={(e) =>
+                  setUsername(e.target.value.replace(/[^a-zA-Z0-9]/g, ""))
+                }
+              /> */}
+              <Input
+                type="text"
+                disabledBy={!editLink}
+                padding={"sm"}
+                value={qrUrl}
+                placeholder={"a123"}
+                onChange={(e) => setQrUrl(e.target.value)}
+              />
+              {editLink ? (
+                <Button
+                  type={"primary"}
+                  padding={"sm"}
+                  callback={() => setEditLink(false)}
+                >
+                  lưu
+                </Button>
+              ) : (
+                <Button
+                  type={"primary"}
+                  padding={"sm"}
+                  callback={() => setEditLink(true)}
+                >
+                  edit
+                </Button>
+              )}
+            </div>
           </div>
         </div>
       </div>
@@ -371,7 +438,7 @@ const Config = () => {
             <span>VNPost</span>
           </div>
           <div className="w-4/6 relative">
-            <Input size={"w-full"} padding={"sm"} />
+            <Input size={"w-full"} padding={"sm"} placeholder={123} />
             <Button
               icon={copy}
               size={"absolute top-1 right-2"}
