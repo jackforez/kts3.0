ceeab9b81ab27c701200332d2e0884ae928471d1
JackForez <jackforez@gmail.com>
2023-07-17 12:47:25 +0700

    v

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,13 +3,15 @@ import { Button, Input } from "../components";
 import { search } from "../ultis/svgs";
 import { ktsRequest } from "../ultis/connections";
 import { toast } from "react-toastify";
-
+import { vnpStatus } from "../ultis/vnpstatus";
 const About = ({ slogan, description }) => {
   const [openResult, setOpenResult] = useState(false);
   const [title, setTitle] = useState("Tra cứu đơn hàng");
   const [loading, setLoading] = useState(false);
   const [orderDetails, setOrderDetails] = useState({});
   const [orderID, setOrderID] = useState("");
+  const [isKst, setIsKts] = useState(false);
+
   const handleClick = () => {
     if (!orderID) {
       toast.warn("Vui lòng nhập mã vận đơn!", {
@@ -20,7 +22,7 @@ const About = ({ slogan, description }) => {
     setLoading(true);
     const config = {
       method: "get",
-      url: `tracking/${orderID}`,
+      url: `v2/tracking/${orderID}`,
       params: {
         id: orderID,
       },
@@ -28,15 +30,16 @@ const About = ({ slogan, description }) => {
 
     ktsRequest(config)
       .then(function (response) {
-        console.log(response);
+        // console.log(response);
         setOrderDetails(response.data.data);
-
+        setIsKts(response.data.kts);
         setLoading(false);
         setOpenResult(!openResult);
       })
       .catch(function (error) {
+        // console.log(error);
         setLoading(false);
-        toast.error("Mã vận đơn đang được xử lý!");
+        toast.warn("Mã vận đơn đang được xử lý, vui lòng thử lại sau!");
       });
   };
   return (
@@ -81,7 +84,9 @@ const About = ({ slogan, description }) => {
             } shadow bg-gray-300`}
           >
             <div className="w-full flex justify-between items-center">
-              <h3 className="pl-3 uppercase">mã vận đơn: {orderID}</h3>
+              <h3 className="pl-3 uppercase font-semibold">
+                mã vận đơn: {orderID}
+              </h3>
               <button
                 className="bg-primary-600 p-2"
                 onClick={() => setOpenResult(false)}
@@ -102,69 +107,262 @@ const About = ({ slogan, description }) => {
                 </svg>
               </button>
             </div>
-            <div className="bg-white h-[70vh]">
-              <div className=" py-4 z-50 flex w-full items-center justify-center backdrop-blur duration-500 ease-in-out">
-                <div className="">
-                  <div className="space-y-2 overflow-auto p-3 text-ktsPrimary">
-                    <div className="col-span-12 space-y-3  px-4 sm:col-span-9">
-                      {orderDetails.TBL_DELIVERY?.length > 0 ? (
-                        orderDetails.TBL_DELIVERY.map((i, index) => {
-                          return (
-                            <div
-                              key={index}
-                              className="col-span-12 border-l-4 border-orange-500 bg-red-100  px-4  sm:col-span-8 sm:space-y-8 sm:before:absolute sm:before:top-2 sm:before:bottom-0 sm:before:-left-3 sm:before:w-0.5"
-                            >
-                              <div className="flex flex-col">
-                                <h6 className="font-semibold tracking-wide">
-                                  {i.STATUSTEXT}
-                                </h6>
-                                <p className="text-xs uppercase tracking-wide">
-                                  {i.NGAY_PHAT}
-                                </p>
-                                <p>{i.VI_TRI}</p>
+            {/* {isKst.kts ? (
+              <div className="bg-white h-[70vh]">
+                <div className=" py-4 z-50 flex w-full items-center justify-center backdrop-blur duration-500 ease-in-out">
+                  <div className="">
+                    <div className="space-y-2 overflow-auto p-3 text-ktsPrimary">
+                      <div className="col-span-12 space-y-3  px-4 sm:col-span-9">
+                        {orderDetails.TBL_DELIVERY?.length > 0 ? (
+                          orderDetails.TBL_DELIVERY.map((i, index) => {
+                            return (
+                              <div
+                                key={index}
+                                className="col-span-12 border-l-4 border-orange-500 bg-red-100  px-4  sm:col-span-8 sm:space-y-8 sm:before:absolute sm:before:top-2 sm:before:bottom-0 sm:before:-left-3 sm:before:w-0.5"
+                              >
+                                <div className="flex flex-col">
+                                  <h6 className="font-semibold tracking-wide">
+                                    {i.STATUSTEXT}
+                                  </h6>
+                                  <p className="text-xs uppercase tracking-wide">
+                                    {i.NGAY_PHAT}
+                                  </p>
+                                  <p>{i.VI_TRI}</p>
+                                </div>
                               </div>
-                            </div>
-                          );
-                        })
-                      ) : (
-                        <h3>Không có dữ liệu trạng thái</h3>
-                      )}
+                            );
+                          })
+                        ) : (
+                          <h3>Đơn hàng KTS</h3>
+                        )}
+                      </div>
                     </div>
-                  </div>
-                  <div className="flex items-start justify-between rounded-t border-b px-7">
-                    <h3 className="text-xl font-semibold text-gray-900 ">
-                      Chi tiết bill gửi
-                    </h3>
-                  </div>
-                  <div className="h-[50vh] overflow-auto overflow-y-auto p-3 text-ktsPrimary">
-                    <div className="col-span-12 space-y-2  px-4 sm:col-span-9">
-                      {orderDetails.TBL_DINH_VI ? (
-                        orderDetails.TBL_DINH_VI.map((i, index) => {
-                          return (
-                            <div
-                              key={index}
-                              className="col-span-12 border-l-4 border-orange-500 bg-red-100  px-4 sm:col-span-8 sm:space-y-8 sm:before:absolute sm:before:top-2 sm:before:bottom-0 sm:before:-left-3 sm:before:w-0.5"
-                            >
-                              <div className="flex flex-col">
-                                <h6 className="font-semibold tracking-wide">
-                                  {i.StatusText}
-                                </h6>
-                                <p className="text-xs uppercase tracking-wide ">
-                                  {i.TraceDate}
-                                </p>
-                                <p>{i.DiaChiBuuCuc}</p>
+                    <div className="flex items-start justify-between rounded-t border-b px-7">
+                      <h3 className="text-xl font-semibold text-gray-900 ">
+                        Chi tiết bill gửi
+                      </h3>
+                    </div>
+                    <div className="h-[50vh] overflow-auto overflow-y-auto p-3 text-ktsPrimary">
+                      <div className="col-span-12 space-y-2  px-4 sm:col-span-9">
+                        {orderDetails.TBL_DINH_VI ? (
+                          orderDetails.TBL_DINH_VI.map((i, index) => {
+                            return (
+                              <div
+                                key={index}
+                                className="col-span-12 border-l-4 border-orange-500 bg-red-100  px-4 sm:col-span-8 sm:space-y-8 sm:before:absolute sm:before:top-2 sm:before:bottom-0 sm:before:-left-3 sm:before:w-0.5"
+                              >
+                                <div className="flex flex-col">
+                                  <h6 className="font-semibold tracking-wide">
+                                    {i.StatusText}
+                                  </h6>
+                                  <p className="text-xs uppercase tracking-wide ">
+                                    {i.TraceDate}
+                                  </p>
+                                  <p>{i.DiaChiBuuCuc}</p>
+                                </div>
                               </div>
-                            </div>
-                          );
-                        })
-                      ) : (
-                        <h3>Không có dữ liệu chi tiết</h3>
-                      )}
+                            );
+                          })
+                        ) : (
+                          <h3>KTS</h3>
+                        )}
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
-            </div>
+            ) : (
+              <div className="bg-white h-[70vh]">
+                <div className=" py-4 z-50 flex w-full items-center justify-center backdrop-blur duration-500 ease-in-out">
+                  <div className="">
+                    <div className="space-y-2 overflow-auto p-3 text-ktsPrimary">
+                      <div className="col-span-12 space-y-3  px-4 sm:col-span-9">
+                        {orderDetails.TBL_DELIVERY?.length > 0 ? (
+                          orderDetails.TBL_DELIVERY.map((i, index) => {
+                            return (
+                              <div
+                                key={index}
+                                className="col-span-12 border-l-4 border-orange-500 bg-red-100  px-4  sm:col-span-8 sm:space-y-8 sm:before:absolute sm:before:top-2 sm:before:bottom-0 sm:before:-left-3 sm:before:w-0.5"
+                              >
+                                <div className="flex flex-col">
+                                  <h6 className="font-semibold tracking-wide">
+                                    {i.STATUSTEXT}
+                                  </h6>
+                                  <p className="text-xs uppercase tracking-wide">
+                                    {i.NGAY_PHAT}
+                                  </p>
+                                  <p>{i.VI_TRI}</p>
+                                </div>
+                              </div>
+                            );
+                          })
+                        ) : (
+                          <h3>Không có dữ liệu trạng thái</h3>
+                        )}
+                      </div>
+                    </div>
+                    <div className="flex items-start justify-between rounded-t border-b px-7">
+                      <h3 className="text-xl font-semibold text-gray-900 ">
+                        Chi tiết bill gửi
+                      </h3>
+                    </div>
+                    <div className="h-[50vh] overflow-auto overflow-y-auto p-3 text-ktsPrimary">
+                      <div className="col-span-12 space-y-2  px-4 sm:col-span-9">
+                        {orderDetails.TBL_DINH_VI ? (
+                          orderDetails.TBL_DINH_VI.map((i, index) => {
+                            return (
+                              <div
+                                key={index}
+                                className="col-span-12 border-l-4 border-orange-500 bg-red-100  px-4 sm:col-span-8 sm:space-y-8 sm:before:absolute sm:before:top-2 sm:before:bottom-0 sm:before:-left-3 sm:before:w-0.5"
+                              >
+                                <div className="flex flex-col">
+                                  <h6 className="font-semibold tracking-wide">
+                                    {i.StatusText}
+                                  </h6>
+                                  <p className="text-xs uppercase tracking-wide ">
+                                    {i.TraceDate}
+                                  </p>
+                                  <p>{i.DiaChiBuuCuc}</p>
+                                </div>
+                              </div>
+                            );
+                          })
+                        ) : (
+                          <h3>
+                            Mã vận đơn đang được xử lý, vui lòng thử lại sau
+                            hoặc liên hệ bộ phận CSKH
+                          </h3>
+                        )}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            )} */}
+            {isKst ? (
+              <div className="bg-white h-[70vh]">
+                <div className=" py-4 z-50 flex w-full items-center justify-center backdrop-blur duration-500 ease-in-out">
+                  <div className="">
+                    <div className="col-span-12 space-y-3  p-4 sm:col-span-9">
+                      <span>Trạng thái cuối cùng: </span>
+                      <span className="uppercase font-semibold">
+                        {
+                          vnpStatus[
+                            orderDetails[0].statusHistory.reverse().pop()
+                              .statusCode || 10
+                          ]
+                        }
+                      </span>
+                    </div>
+                    <div className="flex items-start justify-between rounded-t border-b px-7">
+                      <h3 className="text-xl font-semibold text-gray-900 ">
+                        Chi tiết bill gửi
+                      </h3>
+                    </div>
+                    <div className="h-[60vh] overflow-auto overflow-y-auto p-3 text-ktsPrimary">
+                      <div className="col-span-12 sm:col-span-9">
+                        {orderDetails[0].statusHistory.length > 0 ? (
+                          orderDetails[0].statusHistory
+                            .reverse()
+                            .map((i, index) => {
+                              return (
+                                <div
+                                  key={index}
+                                  className="col-span-12 border-l-4 border-gray-300 py-1 relative px-4 sm:col-span-8 sm:space-y-8 sm:before:absolute sm:before:top-2 sm:before:bottom-0 sm:before:-left-3 sm:before:w-0.5"
+                                >
+                                  <div className="w-4 h-4 bg-green-500 rounded-full absolute top-[50%] -left-2.5"></div>
+                                  <div className="flex flex-col pl-4">
+                                    <h6 className="font-semibold tracking-wide">
+                                      {vnpStatus[i.statusCode || 10]}
+                                    </h6>
+                                    <p className="text-xs uppercase tracking-wide">
+                                      {i.createHour + " - " + i.createdDate}
+                                    </p>
+                                    <p>{i.statusName}</p>
+                                  </div>
+                                </div>
+                              );
+                            })
+                        ) : (
+                          <h3>
+                            Đơn hàng đang được xử lý, vui lòng thử lại sau hoặc
+                            liên hệ bộ phận CSKH
+                          </h3>
+                        )}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ) : (
+              <div className="bg-white h-[70vh]">
+                <div className=" py-4 z-50 flex w-full items-center justify-center backdrop-blur duration-500 ease-in-out">
+                  <div className="">
+                    <div className="space-y-2 overflow-auto p-3 text-ktsPrimary">
+                      <div className="col-span-12 space-y-3  px-4 sm:col-span-9">
+                        {orderDetails.TBL_DELIVERY?.length > 0 ? (
+                          orderDetails.TBL_DELIVERY.map((i, index) => {
+                            return (
+                              <div
+                                key={index}
+                                className="col-span-12 border-l-4 border-orange-500 bg-red-100 px-4  sm:col-span-8 sm:space-y-8 sm:before:absolute sm:before:top-2 sm:before:bottom-0 sm:before:-left-3 sm:before:w-0.5"
+                              >
+                                <div className="flex flex-col">
+                                  <h6 className="font-semibold tracking-wide">
+                                    {i.STATUSTEXT}
+                                  </h6>
+                                  <p className="text-xs uppercase tracking-wide">
+                                    {i.NGAY_PHAT}
+                                  </p>
+                                  <p>{i.VI_TRI}</p>
+                                </div>
+                              </div>
+                            );
+                          })
+                        ) : (
+                          <h3>Không có dữ liệu trạng thái</h3>
+                        )}
+                      </div>
+                    </div>
+                    <div className="flex items-start justify-between rounded-t border-b px-7">
+                      <h3 className="text-xl font-semibold text-gray-900 ">
+                        Chi tiết bill gửi
+                      </h3>
+                    </div>
+                    <div className="h-[50vh] overflow-auto overflow-y-auto p-3 text-ktsPrimary">
+                      <div className="col-span-12 px-4 sm:col-span-9">
+                        {orderDetails.TBL_DINH_VI ? (
+                          orderDetails.TBL_DINH_VI.map((i, index) => {
+                            return (
+                              <div
+                                key={index}
+                                className="col-span-12 border-l-4 border-gray-300 py-2 relative px-4 sm:col-span-8"
+                              >
+                                <div className="w-4 h-4 bg-green-500 rounded-full absolute top-[30%] -left-2.5"></div>
+                                <div className="flex flex-col pr-3">
+                                  <h6 className="font-semibold tracking-wide">
+                                    {i.StatusText}
+                                  </h6>
+                                  <p className="text-xs uppercase tracking-wide ">
+                                    {i.TraceDate}
+                                  </p>
+                                  <p>{i.DiaChiBuuCuc}</p>
+                                </div>
+                              </div>
+                            );
+                          })
+                        ) : (
+                          <h3>
+                            Mã vận đơn đang được xử lý, vui lòng thử lại sau
+                            hoặc liên hệ bộ phận CSKH
+                          </h3>
+                        )}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            )}
           </div>
         </div>
       </div>
---
+++ src/ultis/vnpstatus.js
@@ -0,0 +1,56 @@
+export const vnpStatus = {
+  0: "Lưu nháp",
+  1: "Đã tạo",
+  2: "Chuyển tin cho bưu tá",
+  3: "Đang xử lý",
+  4: "Đang lấy hàng",
+  5: "Lấy hàng không thành công",
+  6: "Lấy hàng thành công",
+  7: "Bưu cục đã nhận hàng",
+  8: "Lấy hàng thất bại",
+  9: "Hủy thu gom",
+
+  10: "Đang vận chuyển",
+  11: "Đã đến Bưu cục phát",
+
+  12: "Đang giao hàng",
+
+  13: "Chuyển tiếp",
+
+  14: "Đã giao hàng",
+
+  15: "Giao hàng không thành công",
+
+  16: "Chờ để chuyển hoàn",
+
+  17: "Đã duyệt hoàn",
+
+  18: "Bắt đầu chuyển hoàn",
+
+  19: "Hoàn thành công",
+
+  20: "Đã trả hàng 1 phần",
+
+  21: "Đã trả tiền COD",
+
+  22: "Hủy giao hàng",
+  101: "EMA - Đã chấp nhận gửi",
+  102: "EMB - Đến Trung tâm khai thác quốc tế nước gửi",
+
+  103: "EMC - Đã rời khỏi Trung tâm khai thác quốc tế nước gửi",
+  104: "EMD - Đã đến Trung tâm khai thác quốc tế nước nhận",
+
+  105: "EME - Đang lưu giữ tại hải quan nước nhận",
+
+  106: "EMF - Ra khỏi Trung tâm khai thác quốc tế của nước nhận",
+
+  107: "EMG - Đã đến Bưu cục phát",
+
+  108: "EMH - Phát chưa thành công",
+
+  109: "EMI - Đã phát thành công",
+
+  110: "EMJ - Đến Trung tâm khai thác quốc tế quá cảnh",
+
+  111: "EMK - Khởi hành từ Trung tâm khai thác quốc tế quá cảnh",
+};
