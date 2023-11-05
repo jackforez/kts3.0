b9eefd5a1567200198023dd03b439efa4a1cc339
JackForez <jackforez@gmail.com>
2023-11-05 21:46:53 +0700

    v

--- src/pages/Viettel.jsx
+++ src/pages/Viettel.jsx
@@ -1,22 +1,42 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { Button } from "../components";
+import { excel } from "../ultis/svgs";
 const Viettel = () => {
+  const [file, setFile] = useState({});
+  console.log(file);
   return (
-    <div className="bg-black">
-      <svg
-        version="1.0"
-        xmlns="http://www.w3.org/2000/svg"
-        width="641.000000pt"
-        height="247.000000pt"
-        viewBox="0 0 641.000000 247.000000"
-        preserveAspectRatio="xMidYMid meet"
-      >
-        <g
-          transform="translate(0.000000,247.000000) scale(0.100000,-0.100000)"
-          fill="#f1f1f1"
-          stroke="none"
-        ></g>
-      </svg>
+    // content
+    <div className="p-3">
+      {/* header */}
+      <div className="flex justify-between h-full">
+        <div className="w-1/2 bg-white p-1 rounded">
+          <input
+            type="file"
+            className="hidden"
+            id="myip"
+            accept=".xlsx, .xls, .csv"
+            onChange={(e) => setFile(e.target.files[0])}
+          />
+          <Button
+            type="primary"
+            padding={"sm"}
+            callback={() => {
+              document.getElementById("myip").click();
+            }}
+          >
+            Chọn file
+          </Button>
+          <span className="px-3">
+            {file?.name || "Chưa có file nào được chọn"}
+          </span>
+        </div>
+        {/* <div className="space-x-3"> */}
+        <Button>Tải file mẫu</Button>
+        {/* <Button>Xuất file</Button>
+        </div> */}
+      </div>
+      {/* main */}
+      <div>abc</div>
     </div>
   );
 };
