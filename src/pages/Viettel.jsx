fd24e70049f10f11e0ca2fe68ce3a372ad812563
JackForez <jackforez@gmail.com>
2024-01-22 22:41:58 +0700

    v

--- src/pages/Viettel.jsx
+++ src/pages/Viettel.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect } from "react";
 import { Button, Input, Selector } from "../components";
 import { download, excel, upload } from "../ultis/svgs";
 import { toast } from "react-toastify";
@@ -6,7 +6,6 @@ import { ktsRequest } from "../ultis/connections";
 import * as XLSX from "xlsx";
 import { loaded, onLoading } from "../redux/systemSlice";
 import { useDispatch, useSelector } from "react-redux";
-import { json } from "react-router-dom";
 const exceptFileTypes = [
   "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
   "application/vnd.ms-excel",
@@ -87,82 +86,76 @@ const ExcelDisplay = ({ jsonData }) => {
             )}
           </thead>
           <tbody className="">
-            {jsonData.slice(7).map(
-              (row, rowIndex) =>
-                row[2] &&
-                row[3] && (
-                  <tr
-                    className={`w-full ${
-                      row[1] ? "bg-green-200" : "bg-red-100"
-                    }`}
-                    key={rowIndex}
-                  >
-                    <td className="w-[40px] text-center border border-gray-200">
-                      {row[0]}
-                    </td>
-                    <td className="w-[160px] text-center border border-gray-200">
-                      {row[1]}
-                    </td>
-                    <td className="w-[160px] text-center border border-gray-200">
-                      {row[2]}
-                    </td>
-                    <td className="w-[160px] text-center border border-gray-200">
-                      {row[3]}
-                    </td>
-                    <td className="w-[340px] text-center border border-gray-200">
-                      {row[4]}
-                    </td>
-                    <td className="w-[340px] text-center border border-gray-200">
-                      {row[5]}
-                    </td>
-                    <td className="w-[120px] text-center border border-gray-200">
-                      {row[6]}
-                    </td>
-                    <td className="w-[160px] text-center border border-gray-200">
-                      {row[7]}
-                    </td>
-                    <td className="w-[160px] text-center border border-gray-200">
-                      {row[8]}
-                    </td>
-                    <td className="w-[160px] text-center border border-gray-200">
-                      {row[9]}
-                    </td>
-                    <td className="w-[120px] text-center border border-gray-200">
-                      {row[10]}
-                    </td>
-                    <td className="w-[200px] text-center border border-gray-200">
-                      {row[11]}
-                    </td>
-                    <td className="w-[140px] text-center border border-gray-200">
-                      {row[12]}
-                    </td>
-                    <td className="w-[120px] text-center border border-gray-200">
-                      {row[13]}
-                    </td>
-                    <td className="w-[120px] text-center border border-gray-200">
-                      {row[14]}
-                    </td>
-                    <td className="w-[60px] text-center border border-gray-200">
-                      {row[15]}
-                    </td>
-                    <td className="w-[60px] text-center border border-gray-200">
-                      {row[16]}
-                    </td>
-                    <td className="w-[60px] text-center border border-gray-200">
-                      {row[17]}
-                    </td>
-                    <td className="w-[120px] text-center border border-gray-200">
-                      {row[18]}
-                    </td>
-                    <td className="w-[160px] text-center border border-gray-200">
-                      {row[19]}
-                    </td>
-                    <td className="w-[140px] text-center border border-gray-200">
-                      {row[20]}
-                    </td>
-                  </tr>
-                )
-            )}
+            {jsonData.slice(7).map((row, rowIndex) => (
+              <tr
+                className={`w-full ${row[1] ? "bg-green-200" : "bg-red-100"}`}
+                key={rowIndex}
+              >
+                <td className="w-[40px] text-center border border-gray-200">
+                  {row[0]}
+                </td>
+                <td className="w-[160px] text-center border border-gray-200">
+                  {row[1]}
+                </td>
+                <td className="w-[160px] text-center border border-gray-200">
+                  {row[2]}
+                </td>
+                <td className="w-[160px] text-center border border-gray-200">
+                  {row[3]}
+                </td>
+                <td className="w-[340px] text-center border border-gray-200">
+                  {row[4]}
+                </td>
+                <td className="w-[340px] text-center border border-gray-200">
+                  {row[5]}
+                </td>
+                <td className="w-[120px] text-center border border-gray-200">
+                  {row[6]}
+                </td>
+                <td className="w-[160px] text-center border border-gray-200">
+                  {row[7]}
+                </td>
+                <td className="w-[160px] text-center border border-gray-200">
+                  {row[8]}
+                </td>
+                <td className="w-[160px] text-center border border-gray-200">
+                  {row[9]}
+                </td>
+                <td className="w-[120px] text-center border border-gray-200">
+                  {row[10]}
+                </td>
+                <td className="w-[200px] text-center border border-gray-200">
+                  {row[11]}
+                </td>
+                <td className="w-[140px] text-center border border-gray-200">
+                  {row[12]}
+                </td>
+                <td className="w-[120px] text-center border border-gray-200">
+                  {row[13]}
+                </td>
+                <td className="w-[120px] text-center border border-gray-200">
+                  {row[14]}
+                </td>
+                <td className="w-[60px] text-center border border-gray-200">
+                  {row[15]}
+                </td>
+                <td className="w-[60px] text-center border border-gray-200">
+                  {row[16]}
+                </td>
+                <td className="w-[60px] text-center border border-gray-200">
+                  {row[17]}
+                </td>
+                <td className="w-[120px] text-center border border-gray-200">
+                  {row[18]}
+                </td>
+                <td className="w-[160px] text-center border border-gray-200">
+                  {row[19]}
+                </td>
+                <td className="w-[140px] text-center border border-gray-200">
+                  {row[20]}
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
@@ -175,57 +168,25 @@ const Viettel = () => {
   const { loading } = useSelector((state) => state.system);
   const { token } = currentUser || "";
   const [file, setFile] = useState({});
-  const [cities, setCities] = useState([]);
-  const [districts, setDistricts] = useState([]);
-  const [wards, setWards] = useState([]);
-  const [toWard, setToWard] = useState("");
-  const [toDistrict, setToDistrict] = useState("");
-  const [toCity, setToCity] = useState("");
-  const [cityName, setCityName] = useState("");
-  const [districtName, setDistrictName] = useState("");
-  const [wardName, setWardName] = useState("");
-  const [getter, setGetter] = useState({});
   const [jsonData, setJsonData] = useState(null);
   // submit state
-  const [excelData, setExcelData] = useState(null);
   const dispatch = useDispatch();
-  const handelChangeGetter = (e) => {
-    setGetter((prev) => {
-      return { ...prev, [e.target.name]: e.target.value };
-    });
-  };
+  console.log(jsonData);
+  // useEffect(() => {
+  //   jsonData &&
+  //     setJsonData(
+  //       jsonData.slice(
+  //         0,
+  //         7 + jsonData.slice(7).indexOf(jsonData.slice(7).find((e) => !e[2]))
+  //       )
+  //     );
+  // }, [file]);
   const handleCreateByExcel = async () => {
     dispatch(onLoading());
-    // if (!getter.phone || getter.phone.length != 10) {
-    //   toast.warn("Số điện thoại người nhận hàng không hợp lệ");
-    //   dispatch(loaded());
-    //   return;
-    // }
-    // if (!getter.name) {
-    //   toast.warn("Chưa nhập tên người nhận hàng");
-    //   dispatch(loaded());
-    //   return;
-    // }
-
-    // if (!getter.address) {
-    //   toast.warn("Chưa nhập địa chỉ người nhận hàng");
-    //   dispatch(loaded());
-    //   return;
-    // }
-    // if (!weight || weight <= 0) {
-    //   toast.warn("Khối lượng không hợp lệ");
-    //   dispatch(loaded());
-    //   return;
-    // }
-    // if (!itemName) {
-    //   toast.warn("Cần nhập nội dung hàng hóa");
-    //   dispatch(loaded());
-    //   return;
-    // }
-    // if (qty < 1) {
-    //   toast.warn("Số lượng không hợp lệ!");
-    //   dispatch(loaded());
-    // }
+    if (!jsonData) {
+      toast.warn("Chưa có file nào được chọn!");
+      dispatch(loaded());
+    }
     try {
       const res = await ktsRequest.post(
         "/v2/bills",
@@ -245,7 +206,14 @@ const Viettel = () => {
           },
         }
       );
-      toast.success(res.data);
+
+      setJsonData((prev) => {
+        return prev.map((i) => {
+          i[2] && (i[1] = res.data.find((j) => j.id == i[0])?.ktsNumber);
+        });
+      });
+      // console.log(jsonData);
+      toast.success("Gửi thông tin thành công!");
       dispatch(loaded());
     } catch (err) {
       toast.error(
@@ -277,93 +245,19 @@ const Viettel = () => {
           // defval: "",
           // blankrows: true,
         });
-
-        setJsonData(excelData);
+        setJsonData(
+          excelData.slice(
+            0,
+            7 +
+              excelData.slice(7).indexOf(excelData.slice(7).find((e) => !e[2]))
+          )
+        );
       };
 
       reader.readAsBinaryString(file);
     }
   };
-  useEffect(() => {
-    const getCities = async () => {
-      try {
-        const res = await ktsRequest.get("/cities");
-        const data = Object.values(res.data);
-        setCities(data);
-      } catch (error) {
-        toast.error(error);
-      }
-    };
-    getCities();
-  }, []);
-  useEffect(() => {
-    const getDistricts = async () => {
-      try {
-        const cName = cities.find((city) => city.name_with_type == toCity);
-        const resd = await ktsRequest.get(`/cities/districts/${cName.code}`);
-        const data = Object.values(resd.data);
-        setDistricts(data);
-        setCityName(cName.name_with_type);
 
-        data.findIndex((el) => el.name_with_type.includes(toDistrict)) < 0 &&
-          setToDistrict(data[0].name_with_type);
-        setGetter((prev) => {
-          return {
-            ...prev,
-            cityCode: cName.code,
-            cityName: cName.name,
-            cityFullName: cName.name_with_type,
-          };
-        });
-      } catch (error) {
-        toast.error(error);
-      }
-    };
-    getDistricts();
-  }, [toCity]);
-  useEffect(() => {
-    const getWards = async () => {
-      try {
-        const dName = districts.find((d) => d.name_with_type == toDistrict);
-        const resw = await ktsRequest.get(`cities/wards/${dName.code}`);
-        const data = Object.values(resw.data);
-        setWards(data);
-        setDistrictName(dName.name_with_type);
-        data.findIndex((el) => el.name_with_type.includes(toWard)) < 0 &&
-          setToWard(data[0].name_with_type);
-        setGetter((prev) => {
-          return {
-            ...prev,
-            districtCode: dName.code,
-            districtName: dName.name,
-            districtFullName: dName.name_with_type,
-          };
-        });
-      } catch (error) {
-        toast.error(error);
-      }
-    };
-    getWards();
-  }, [toDistrict]);
-  useEffect(() => {
-    const getWard = () => {
-      try {
-        const wName = wards.find((w) => w.name_with_type === toWard);
-        setWardName(wName.name);
-        setGetter((prev) => {
-          return {
-            ...prev,
-            wardCode: wName.code,
-            wardName: wName.name,
-            wardFullName: wName.name_with_type,
-          };
-        });
-      } catch (error) {
-        toast.error(error);
-      }
-    };
-    getWard();
-  }, [toWard]);
   return (
     // content
     <div className="p-3">
@@ -380,7 +274,7 @@ const Viettel = () => {
           />
           <Button
             type="primary"
-            padding={"sm"}
+            padding={"xs"}
             icon={upload}
             callback={() => {
               document.getElementById("myip").click();
@@ -399,6 +293,8 @@ const Viettel = () => {
             loading={loading}
             disabledBy={loading}
             animation={true}
+            padding={"xs"}
+            size="px-4"
           >
             Gửi thông tin
           </Button>
